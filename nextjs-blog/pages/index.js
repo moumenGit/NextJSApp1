fc6fdf8edfb25b05f380901bd4939ffe0c69dad6
moumenGit <30838825+moumenGit@users.noreply.github.com>
2022-04-13 12:42:10 +0300

    Second Commit

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,8 +10,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome Moumen to <a href="https://nextjs.org">Next.js!</a>
+      <h1 className="title">
+          Read{' '}
+          <Link href="/posts/first-post">
+              <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
